e68dc2f4cf615b7bccda46a87d6866829a054754
Hwajun Lee <49684257+Hwajun1323@users.noreply.github.com>
2023-01-04 15:04:19 +0100

    Update correct_bracket.java

--- java/correct_bracket.java
+++ java/correct_bracket.java
@@ -5,23 +5,55 @@ class Solution {
         boolean answer = true;
         int open = 0;
         int close = 0;
-        
+        Stack<Character> stack = new Stack<>();
+
         if (s.charAt(0) == ')') {
             return false;
         }
         
-        String[] sp = s.toLowerCase().split("");
-        //Stack<Character> stack = new Stack<>();
-        
-        for(String c : sp){
-            if(c.equals("(")){
-                open++;
-                answer = false;
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            
+            if (stack.isEmpty()) {
+                    stack.push(c);
             }else{
-                close++;
-                answer = open == close ? true : false;
-            }
+                if(c=='('){
+                    stack.push(c);
+                }else{
+                    if(open == close){
+                        stack.pop();
+                    }else{
+                        stack.push(c);
+                    } 
+                }
+            } 
         }
-        return open == close ? true : false;
+        return stack.isEmpty() ? true : false;
     }
 }
+
+// class Solution {
+//     boolean solution(String s) {
+//         boolean answer = true;
+//         int open = 0;
+//         int close = 0;
+        
+//         if (s.charAt(0) == ')') {
+//             return false;
+//         }
+        
+//         String[] sp = s.toLowerCase().split("");
+//         //Stack<Character> stack = new Stack<>();
+        
+//         for(String c : sp){
+//             if(c.equals("(")){
+//                 open++;
+//                 answer = false;
+//             }else{
+//                 close++;
+//                 answer = open == close ? true : false;
+//             }
+//         }
+//         return open == close ? true : false;
+//     }
+// }
