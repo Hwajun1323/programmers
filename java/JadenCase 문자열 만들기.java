591a3c1c130a1ede3f12c015044217516f514636
Hwajun Lee <49684257+Hwajun1323@users.noreply.github.com>
2023-01-04 13:07:01 +0100

    Update JadenCase 문자열 만들기.java

--- java/JadenCase 문자열 만들기.java
+++ java/JadenCase 문자열 만들기.java
@@ -32,3 +32,18 @@ class Solution {
         return str.charAt(0) >= '0' && str.charAt(0) <= '9';
     }
 }
+
+// class Solution {
+//  public String solution(String s) {
+//        String answer = "";
+//        String[] sp = s.toLowerCase().split("");
+//        boolean flag = true;
+//
+//        for(String ss : sp) {
+//            answer += flag ? ss.toUpperCase() : ss;
+//            flag = ss.equals(" ") ? true : false;
+//        }
+//
+//        return answer;
+//  }
+//}
